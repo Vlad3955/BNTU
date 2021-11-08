b219f4ed73dd210f7ae4f6cda6d944819dad63e4
Vlad3955 <vladreut28@gmail.com>
2021-11-08 06:56:58 +0300

    Added some code in Stopwatch

--- CPPBNTU/KR1/StopWatch/StopWatch/main.cpp
+++ CPPBNTU/KR1/StopWatch/StopWatch/main.cpp
@@ -11,6 +11,19 @@ int main()
 	StopWatch sw;
 
 	sw.Start();
+	cout << "Секундомер запущен" << endl;
+	sw.Show();
 
+	Sleep(3000);
+	sw.Stop();
+	cout << "Секундомер остановлен спустя 3 сек" << endl;
+	sw.Show();
+
+	Sleep(5000);
+	sw.Start();
+	cout << "Секундомер продолжил работу спустя 5 секунд" << endl;
+	sw.Show();
+
+	cout << endl;
 	return 0;
 }
