3aee9a44749fdd24e648e0b4976533c68144a563
Vlad3955 <vladreut28@gmail.com>
2022-04-12 19:33:34 +0300

    added Networks/HW7,some code and docs to BNTU/Networks and BNTU/OOP

--- OOP/LabsOOP/C++/OOP_BNTUCPP/OOP_BNTUCPP/main.cpp
+++ OOP/LabsOOP/C++/OOP_BNTUCPP/OOP_BNTUCPP/main.cpp
@@ -1,161 +1,6 @@
 #include "OOP_Labs.h"
 
-//class Product
-//{
-//public:
-//	string Titel;
-//	int Price;
-//	Product(string t, int p)
-//	{
-//		Titel = t; Price = p;
-//	}
-//	void setTitel(string t)
-//	{
-//		Titel = t;
-//	}
-//	string getTitel()
-//	{
-//		return Titel;
-//	}
-//	void setPrice(int p)
-//	{
-//		Price = p;
-//	}
-//	int getPrice()
-//	{
-//		return Price;
-//	}
-//};
-//
-//
-//
-//class Store
-//{
-//public:
-//	vector<Product> lstProd;
-//	vector<Customer> lstCust;
-//	Customer currentCust;
-//	void AddProduct(string t, int p)
-//	{
-//		lstProd.push_back(Product(t, p));
-//	}
-//	void AddCustomer(string n, CustomerType ct)
-//	{
-//		lstCust.push_back(Customer(n, ct));
-//	}
-//	Customer GetCustByName(string n)
-//	{
-//		for (auto item : lstCust)
-//		{
-//			if (item.Name == n)
-//			{
-//				return item;
-//			}
-//			else
-//			{
-//				cout << "Error";
-//			}
-//		}
-//	}
-//	Product GetProdByTitle(string t)
-//	{
-//		for(auto item : lstProd)
-//		{
-//			if (item.Titel == t)
-//			{
-//				return item;
-//			}
-//			else
-//			{
-//				cout << "Error";
-//			}
-//		}
-//	}
-//	void LogIn(string n)
-//	{
-//		if (currentCust == null)
-//			currentCust = GetCustByName(n);
-//	}
-//    void LogOut()
-//	{
-//		currentCust = nullptr;
-//	}
-//	void BuyProduct(string titel)
-//	{
-//		Product p = GetProdByTitle(titel);
-//		currentCust.BuyProduct(p);
-//	}
-//	int GetTotalSum()
-//	{
-//		int sum = 0;
-//		for(Customer c : lstCust)
-//		{
-//			for(Product p : c.lstProdOfCust)
-//			{
-//				sum += p.Price;
-//			}
-//		}
-//		return sum;
-//	}
-//	string GetProductsByName(string
-//		name)
-//	{
-//		Customer c = GetCustByName(name);
-//		string s = «Customer:» + c.Name +
-//			«\n»;
-//		foreach(Product p in c.LstProdOfCust)
-//		{
-//			s += p.Titel + « « + p.Price +
-//				«\n»;
-//		}
-//		return s;
-//	}
-//};
-//
-//
-//
-//
-//enum CustomerType { VIP, usual };
-//class Customer
-//{
-//public:
-//	vector<Product> lstProdOfCust;
-//	string Name;
-//	CustomerType CType;
-//	Customer(string n, CustomerType ct)
-//	{
-//		Name = n; CType = ct;
-//	}
-//	void setName(string n)
-//	{
-//		Name = n;
-//	}
-//	string getName()
-//	{
-//		return Name;
-//	}
-//	void setCType(CustomerType ct)
-//	{
-//		CType = ct;
-//	}
-//	CustomerType getCType()
-//	{
-//		return CType;
-//	}
-//	void BuyProduct(Product p)
-//	{
-//		lstProdOfCust.push_back(p);
-//	}
-//	int GetSum()
-//	{
-//		int s = 0;
-//		for(Product item : lstProdOfCust)
-//		{
-//			s += item.Price;
-//		}
-//		return s;
-//	}
-//};
+
 
 int main()
 {
@@ -211,7 +56,7 @@ int main()
 	bank.information();*/
 	//=====================================================================================
 	//=====================================================================================
-	// LabWork 1 
+	// LabWork 6 
 	/*Student student("Ivan Ivanov", 17);
 	cout << "Student's name: " << student.getName() << " age: " << student.getAge() << endl;
 	Teacher teacher("Sergey Sergeev", 37);
@@ -219,7 +64,7 @@ int main()
 
 	//=====================================================================================
 	//=====================================================================================
-	// LabWork 2
+	// LabWork 7
 	/*const int N = 256;
 	char line[N];
 	cin.getline(line, N);
