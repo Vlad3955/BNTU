5a636c10f1c000e921b7e9b8f9a411e76f742363
Vlad3955 <vladreut28@gmail.com>
2022-07-09 17:46:38 +0300

    added Course_Project_OOP to BNTU/OOP

--- OOP/Course_Project_OOP/Course_Project_OOP/Course_Project_OOP_Header.h
+++ OOP/Course_Project_OOP/Course_Project_OOP/Course_Project_OOP_Header.h
@@ -5,6 +5,7 @@
 #include <algorithm>
 
 
+
 using namespace std;
 
 // CBookCard
@@ -38,153 +39,153 @@ private:
 //////////////////////////////////////////////////////////////
 
 
-// Hierarchy vegetable
-//============================================================
-// abstract class Vegetable
-class Vegetable
-{
-public:
-	Vegetable();
-	Vegetable(string _name);
-	Vegetable(string _name, double _kcal_per_100g);
-	Vegetable(string _name, double _kcal_per_100g, double _weight);
-	virtual string get_name() const;
-	virtual double get_kcal_per_100g() const;
-	virtual double get_weight() const;
-	virtual void set_category(string _category);
-	virtual string get_category() const;
-	double get_total_calories() const;
-	virtual void all_info() = 0;
-	virtual ~Vegetable();
-protected:
-	string name, category;
-	double kcal_per_100g, weight;
-};
-//============================================================
-
-// CucurbitaceaeVegetable classes
-//============================================================
-class CucurbitaceaeVegetable : public Vegetable
-{
-public:
-	CucurbitaceaeVegetable();
-	CucurbitaceaeVegetable(string _name, double _kcal_per_100g);
-	CucurbitaceaeVegetable(string _name, double _kcal_per_100g, double _weight);
-	void set_harvest_country(string _harvest_country);
-	string get_harvest_country();
-	virtual void all_info() override;
-	~CucurbitaceaeVegetable();
-protected:
-	string harvest_country;
-};
-
-class Cucumber : public CucurbitaceaeVegetable
-{
-public:
-	Cucumber();
-	Cucumber(double _weight);
-	~Cucumber();
-};
-//============================================================
-
-
-// NightshadeVegetable classes
-//============================================================
-class NightshadeVegetable : public Vegetable
-{
-public:
-	NightshadeVegetable();
-	NightshadeVegetable(string _name, double _kcal_per_100g);
-	NightshadeVegetable(string _name, double _kcal_per_100g, double _weight);
-	void set_variety(string _variety);
-	string get_variety();
-	virtual void all_info() override;
-	~NightshadeVegetable();
-protected:
-	string variety;
-};
-
-class Potato : public NightshadeVegetable
-{
-public:
-	Potato();
-	Potato(double _weight);
-	~Potato();
-};
-
-class Tomato : public NightshadeVegetable
-{
-public:
-	Tomato();
-	Tomato(double _weight);
-	~Tomato();
-};
-//============================================================
-
-// RootVegetable classes
-//============================================================
-class RootVegetable : public Vegetable
-{
-public:
-	RootVegetable();
-	RootVegetable(string _name, double _kcal_per_100g);
-	RootVegetable(string _name, double _kcal_per_100g, double _weight);
-	void set_crop_year(string _crop_year);
-	string get_crop_year();
-	virtual void all_info() override;
-	~RootVegetable();
-protected:
-	string crop_year;
-};
-
-class Carrot : public RootVegetable
-{
-public:
-	Carrot();
-	Carrot(double _weight);
-	~Carrot();
-};
-
-class Beet : public RootVegetable
-{
-public:
-	Beet();
-	Beet(double _weight);
-	~Beet();
-};
-//============================================================
-
-// Class Flavoring
-//============================================================
-//class Flavoring
+//// Hierarchy vegetable
+////============================================================
+//// abstract class Vegetable
+//class Vegetable
 //{
 //public:
-//	Flavoring();
-//	Flavoring(string _name, double _kcal_per_100g, double _weight);
-//	string get_name() const;
-//	double get_kcal_per_100g() const;
-//	double get_weight() const;
+//	Vegetable();
+//	Vegetable(string _name);
+//	Vegetable(string _name, double _kcal_per_100g);
+//	Vegetable(string _name, double _kcal_per_100g, double _weight);
+//	virtual string get_name() const;
+//	virtual double get_kcal_per_100g() const;
+//	virtual double get_weight() const;
+//	virtual void set_category(string _category);
+//	virtual string get_category() const;
 //	double get_total_calories() const;
-//	~Flavoring();
-//private:
-//	string name;
+//	virtual void all_info() = 0;
+//	virtual ~Vegetable();
+//protected:
+//	string name, category;
 //	double kcal_per_100g, weight;
 //};
-//============================================================
-
-
-class Salad
-{
-public:
-	Salad();
-	double get_total_calories() const;
-	void add_ingredients(Vegetable * veg);
-	void about_salad();
-	~Salad();
-private:
-	vector<Vegetable*> salad;
-	//Flavoring flavoring;
-};
+////============================================================
+//
+//// CucurbitaceaeVegetable classes
+////============================================================
+//class CucurbitaceaeVegetable : public Vegetable
+//{
+//public:
+//	CucurbitaceaeVegetable();
+//	CucurbitaceaeVegetable(string _name, double _kcal_per_100g);
+//	CucurbitaceaeVegetable(string _name, double _kcal_per_100g, double _weight);
+//	void set_harvest_country(string _harvest_country);
+//	string get_harvest_country();
+//	virtual void all_info() override;
+//	~CucurbitaceaeVegetable();
+//protected:
+//	string harvest_country;
+//};
+//
+//class Cucumber : public CucurbitaceaeVegetable
+//{
+//public:
+//	Cucumber();
+//	Cucumber(double _weight);
+//	~Cucumber();
+//};
+////============================================================
+//
+//
+//// NightshadeVegetable classes
+////============================================================
+//class NightshadeVegetable : public Vegetable
+//{
+//public:
+//	NightshadeVegetable();
+//	NightshadeVegetable(string _name, double _kcal_per_100g);
+//	NightshadeVegetable(string _name, double _kcal_per_100g, double _weight);
+//	void set_variety(string _variety);
+//	string get_variety();
+//	virtual void all_info() override;
+//	~NightshadeVegetable();
+//protected:
+//	string variety;
+//};
+//
+//class Potato : public NightshadeVegetable
+//{
+//public:
+//	Potato();
+//	Potato(double _weight);
+//	~Potato();
+//};
+//
+//class Tomato : public NightshadeVegetable
+//{
+//public:
+//	Tomato();
+//	Tomato(double _weight);
+//	~Tomato();
+//};
+////============================================================
+//
+//// RootVegetable classes
+////============================================================
+//class RootVegetable : public Vegetable
+//{
+//public:
+//	RootVegetable();
+//	RootVegetable(string _name, double _kcal_per_100g);
+//	RootVegetable(string _name, double _kcal_per_100g, double _weight);
+//	void set_crop_year(string _crop_year);
+//	string get_crop_year();
+//	virtual void all_info() override;
+//	~RootVegetable();
+//protected:
+//	string crop_year;
+//};
+//
+//class Carrot : public RootVegetable
+//{
+//public:
+//	Carrot();
+//	Carrot(double _weight);
+//	~Carrot();
+//};
+//
+//class Beet : public RootVegetable
+//{
+//public:
+//	Beet();
+//	Beet(double _weight);
+//	~Beet();
+//};
+////============================================================
+//
+//// Class Flavoring
+////============================================================
+////class Flavoring
+////{
+////public:
+////	Flavoring();
+////	Flavoring(string _name, double _kcal_per_100g, double _weight);
+////	string get_name() const;
+////	double get_kcal_per_100g() const;
+////	double get_weight() const;
+////	double get_total_calories() const;
+////	~Flavoring();
+////private:
+////	string name;
+////	double kcal_per_100g, weight;
+////};
+////============================================================
+//
+//
+//class Salad
+//{
+//public:
+//	Salad();
+//	double get_total_calories() const;
+//	void add_ingredients(Vegetable * veg);
+//	void about_salad();
+//	~Salad();
+//private:
+//	vector<Vegetable*> salad;
+//	//Flavoring flavoring;
+//};
 
 
 
--- OOP/Course_Project_OOP/Course_Project_OOP/Course_Project_OOP_Main.cpp
+++ OOP/Course_Project_OOP/Course_Project_OOP/Course_Project_OOP_Main.cpp
@@ -27,50 +27,52 @@ int main()
 	for_each(vec.begin(), vec.end(), [&](const CBookCard& a) {cout << a.book_description(); });*/
 	//============================================================
 
-	// Hierarchy vegetable
-    //============================================================
-	Carrot carrot(100);
-	Beet beet(100);
-	Potato potato(100);
-	Tomato tomato(100);
-	Cucumber cucumber(100);
-	vector<Vegetable*> salad;
-	/*salad.push_back(&carrot);
-	salad.push_back(&beet);
-	salad.push_back(&potato);
-	salad.push_back(&tomato);
-	salad.push_back(&cucumber);
-	double sum = 0.0;
-	for (auto& it : salad)
-	{
-		sum += it->get_total_calories();
-	}
-	cout << sum;*/
+	//// Hierarchy vegetable
+ //   //============================================================
+	//Carrot carrot(100);
+	//Beet beet(100);
+	//Potato potato(100);
+	//Tomato tomato(100);
+	//Cucumber cucumber(100);
+	//vector<Vegetable*> salad;
+	///*salad.push_back(&carrot);
+	//salad.push_back(&beet);
+	//salad.push_back(&potato);
+	//salad.push_back(&tomato);
+	//salad.push_back(&cucumber);
+	//double sum = 0.0;
+	//for (auto& it : salad)
+	//{
+	//	sum += it->get_total_calories();
+	//}
+	//cout << sum;*/
 
-	Salad vinigrette;
-	vinigrette.add_ingredients(&carrot);
-	vinigrette.add_ingredients(&beet);
-	vinigrette.add_ingredients(&potato);
-	vinigrette.add_ingredients(&tomato);
-	vinigrette.add_ingredients(&cucumber);
+	//Salad vinigrette;
+	//vinigrette.add_ingredients(&carrot);
+	//vinigrette.add_ingredients(&beet);
+	//vinigrette.add_ingredients(&potato);
+	//vinigrette.add_ingredients(&tomato);
+	//vinigrette.add_ingredients(&cucumber);
 
-	vinigrette.about_salad();
+	//vinigrette.about_salad();
 
-	/*int size = 5;
-	Vegetable** arrVeg = new Vegetable*[size];
-	arrVeg[0] = &carrot;
-	arrVeg[1] = &beet;
-	arrVeg[2] = &potato;
-	arrVeg[3] = &tomato;
-	arrVeg[4] = &cucumber;
-	double sum = 0.0;
-	for (size_t i = 0; i < size; i++)
-	{
-		sum += arrVeg[i]->get_total_calories();
-	}
-	cout << sum;
-	delete[] arrVeg;
-	arrVeg = nullptr;*/
-	//============================================================
+	///*int size = 5;
+	//Vegetable** arrVeg = new Vegetable*[size];
+	//arrVeg[0] = &carrot;
+	//arrVeg[1] = &beet;
+	//arrVeg[2] = &potato;
+	//arrVeg[3] = &tomato;
+	//arrVeg[4] = &cucumber;
+	//double sum = 0.0;
+	//for (size_t i = 0; i < size; i++)
+	//{
+	//	sum += arrVeg[i]->get_total_calories();
+	//}
+	//cout << sum;
+	//delete[] arrVeg;
+	//arrVeg = nullptr;*/
+	////============================================================
+
+	
 	return 0;
 }
