6cc1f49e68c3674605a490fb45fd2050f82cb4bc
Vlad3955 <vladreut28@gmail.com>
2024-05-10 11:10:53 +0300

    some mod to DBMS/course_project

--- DBMS/course_project/LumberProduction/LumberProduction/dbms_functions.cpp
+++ DBMS/course_project/LumberProduction/LumberProduction/dbms_functions.cpp
@@ -21,7 +21,7 @@ void LumberDataEntry(LumberData* (&ld), int& n)
         cout << "Ведите через пробел название сырья и количество: ";
         cin >> raw_material_.name >> raw_material_.quantity;
 
-        cout << "Ведите через пробел название проданого продукта, тип сырья, количество и дату: ";
+        cout << "Ведите через пробел название проданого продукта, тип сырья, дату и количество: ";
         cin >> sale_.product.name >> sale_.product.type.name >> sale_.sale_date >> sale_.quantity_sold;
 
         cout << "Ведите название поставщика: ";
@@ -99,5 +99,39 @@ void LumberDataReading(LumberData* (&ld), int& n, string filename)
                     >> deferred_sale_.reason;
             ld[i].LumberDataEntry(product_, raw_material_, sale_, supplier_, deferred_sale_);
         }
+
+        cout << "Данные считаны!" << endl;
+    
     }
+    else
+    {
+        cout << "Ошибка открытия файла!" << endl;
+    }
+
+    reading.close();
+}
+
+void Print(LumberData* ld, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Данные №" << i + 1 << endl;
+
+        ld[i].Print();
+        cout << "_______________________________________________________\n";
+    }
+}
+
+void LumberDataChange(LumberData* ld, int n)
+{
+    Product product_;
+    RawMaterial raw_material_;
+    Sale sale_;
+    Supplier supplier_;
+    DeferredSale deferred_sale_;
+    int _n;
+
+    cout << "Выедите номер нужного элемента (от 1 до " << n << "): ";
+    cin >> _n;
+    _n--;
 }
